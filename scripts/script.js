2d77ac44a886bc462c4d1736006b0cf1438390c5
The1987 <mrchrishantis@gmail.com>
2020-03-26 07:48:39 -0400

    initial project upload

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+
+
+
+// Event Listener #1 - When x button is clicked run function 'closeModal' //
+document.getElementById("x_button").addEventListener("click", closeModal);
+
+// Event Listener #2 - When user clicks on the grey area (body) run 'closeModal' //
+document.body.addEventListener('click', closeModal, true);
+
+function closeModal(event) {
+  // console.log('event', event);
+  // console.log('event', event.target.id);
+
+  if (event.target.id === "wrapper" || event.target.id === "body_id" || event.target.id === "cta-2") {
+    var x = document.getElementById('sub-container');
+    x.style.display = 'none';
+
+    var y = document.getElementById('hidden-container');
+    y.style.display = 'block';
+  }
+}
+
+// Event Listener #3 - When hidden container button is clicked run function 'openModal' //
+document.getElementById("hidden-container").addEventListener("click", openModal);
+
+function openModal() {
+  var y = document.getElementById('hidden-container');
+  y.style.display = 'none';
+
+  var x = document.getElementById('sub-container');
+  x.style.display = 'block';
+}
+
+
+// Form Validation Rule #1 - If input has no value disable button //
+function inputChecker() {
+  if (document.getElementById("email").value === "") {
+    document.getElementById('submit').disabled = true;
+  } else {
+    document.getElementById('submit').disabled = false;
+  }
+}
+
+// Form Validation Rule #2 - Review input text //
+document.getElementById("submit").addEventListener("click", ValidateEmail);
+function ValidateEmail() {
+  var inputText = document.getElementById("email").value
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.match(mailformat)) {
+    return true;
+  }
+  else {
+    document.getElementById("error-messsage").innerHTML = "Please enter in a valid email."
+    return false;
+  }
+}
