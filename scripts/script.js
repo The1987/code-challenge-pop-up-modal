83d603228bcf64954368177956a68300338057e3
The1987 <mrchrishantis@gmail.com>
2020-03-26 08:02:07 -0400

    included x button in event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,12 @@
 
 
-
-// Event Listener #1 - When x button is clicked run function 'closeModal' //
-document.getElementById("x_button").addEventListener("click", closeModal);
-
-// Event Listener #2 - When user clicks on the grey area (body) run 'closeModal' //
+// Event Listener #1 - When user clicks on the grey area (body) run 'closeModal' //
 document.body.addEventListener('click', closeModal, true);
 
 function closeModal(event) {
-  // console.log('event', event);
-  // console.log('event', event.target.id);
+  
 
-  if (event.target.id === "wrapper" || event.target.id === "body_id" || event.target.id === "cta-2") {
+  if (event.target.id === "wrapper" || event.target.id === "body_id" || event.target.id === "cta-2" ||  event.target.id === "x_button") {
     var x = document.getElementById('sub-container');
     x.style.display = 'none';
 
@@ -20,7 +15,7 @@ function closeModal(event) {
   }
 }
 
-// Event Listener #3 - When hidden container button is clicked run function 'openModal' //
+// Event Listener #2 - When hidden container button is clicked run function 'openModal' //
 document.getElementById("hidden-container").addEventListener("click", openModal);
 
 function openModal() {
